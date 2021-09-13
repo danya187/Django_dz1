0f5088f7995a23775c53e0e110d68e8e9a57c8b0
abyazov <abyazov2000@mail.ru>
2021-09-13 21:11:22 +0300

    less5

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,9 +1,8 @@
-from datetime import date
-
-
-
 from django.shortcuts import render
 from mainapp.models import Product
+from django.shortcuts import get_object_or_404
+from mainapp.models import ProductCategory
+
 
 def main(request):
     title = 'Главная'
@@ -12,9 +11,63 @@ def main(request):
     return render(request, 'mainapp/index.html',content)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    def products(request, pk=None):
+        print(pk)
+
+        title = 'продукты'
+        links_menu = ProductCategory.objects.all()
+
+        if pk is not None:
+            if pk == 0:
+                products = Product.objects.all().order_by('price')
+                category = {'name': 'все'}
+            else:
+                category = get_object_or_404(ProductCategory, pk=pk)
+                products = Product.objects.filter(category__pk=pk).order_by('price')
+
+            content = {
+                'title': title,
+                'links_menu': links_menu,
+                'category': category,
+                'products': products,
+            }
+
+            return render(request, 'mainapp/products_list.html', content)
+
+        same_products = Product.objects.all()[3:5]
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'same_products': same_products
+        }
+
+        return render(request, 'mainapp/products.html', content)
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+basket = []
+if request.user.is_authenticated:
+    basket = Basket.objects.filter(user=request.user)
+
+if pk:
+    if pk == '0':
+        products = Product.objects.all().order_by('price')
+        category = {'name': 'все'}
+    else:
+        category = get_object_or_404(ProductCategory, pk=pk)
+        products = Product.objects.filter(category__pk=pk).order_by('price')
+
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'category': category,
+        'products': products,
+        'basket': basket,
+    }
+
+    return render(request, 'mainapp/products_list.html', content)
 
 
