8280f386c38f5a796c6a47d289c392afd42e76e5
abyazov <abyazov2000@mail.ru>
2021-09-15 19:52:14 +0300

    less6

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from mainapp.views import product, contact
 
 urlpatterns = [
+    path('', mainapp.products, name='index'),
     path('product/<int:pk>', product, name ='product'),
     path('contact/', contact, name ='contact'),
 ]
