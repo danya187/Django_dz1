9a25413fdfc0634c114bff78b5d63070ddc65971
abyazov <abyazov2000@mail.ru>
2021-09-28 15:19:27 +0300

    less8

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from mainapp.views import product, contact
+
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
-    path('product/<int:pk>', product, name ='product'),
-    path('contact/', contact, name ='contact'),
+    path('category/<int:pk>/', mainapp.products, name='category'),
+    path('category/<int:pk>/page/<int:page>/', mainapp.products, name='page'),
+    path('product/<int:pk>/', mainapp.product, name='product'),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,7 +3,7 @@ from mainapp.models import Product
 from django.shortcuts import get_object_or_404
 from mainapp.models import ProductCategory
 import random
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def main(request):
     title = 'Главная'
@@ -128,3 +128,40 @@ def product(request, pk):
     }
 
     return render(request, 'mainapp/product.html', content)
+
+
+def products(request, pk=None, page=1):
+    title = 'продукты'
+    links_menu = ProductCategory.objects.filter(is_active=True)
+    basket = getBasket(request.user)
+
+    if pk is not None:
+        if pk == 0:
+            category = {
+                'pk': 0,
+                'name': 'все'
+            }
+            products = Product.objects.filter(is_active=True, \
+                                              category__is_active=True).order_by('price')
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk, \
+                                              is_active=True, category__is_active=True).order_by('price')
+
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products_paginator,
+            'basket': basket,
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
